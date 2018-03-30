ca1a6baa0a9f7eba8b03cbf6f51e0ae3af829fb2
Andre Louw <alouw@retrorabbit.co.za>
2018-03-30 15:38:21 +0200

    Add react-cosmos.

---
+++ config/cosmos.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rootPath: '../',
+  // containerQuerySelector: '#root',
+  webpackConfigPath: './config/webpack.config.dev',
+  publicPath: 'public',
+  // Optional: Add this when you start using proxies
+  // proxiesPath: 'src/cosmos.proxies',
+};
