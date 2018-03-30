1d45917582c6e5ab2a590e9744dc3b801939421c
Andre Louw <alouw@retrorabbit.co.za>
2018-03-30 12:35:48 +0200

    Add storybook.

---
+++ stories/index.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button, Welcome } from '@storybook/react/demo';
+
+import App from '../src/containers/App';
+
+// eslint-disable-next-line
+storiesOf('App', module).add('Home page', () => <App />);
+
+// storiesOf('Button', module)
+//   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+//   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
