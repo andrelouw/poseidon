a38f879e8155eef6a64b4f1908207c8aa227d9d4
Andre Louw <alouw@retrorabbit.co.za>
2018-03-30 14:09:19 +0200

    Add react-css-modules.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import CssModules from 'react-css-modules';
+
 import logo from '../logo.svg';
 import styles from './App.css';
 
@@ -6,12 +8,12 @@ import styles from './App.css';
 class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <header className={styles.header}>
-          <img src={logo} className={styles.logo} alt="logo" />
-          <h1 className={styles.title}>Welcome to React</h1>
+      <div styleName="App">
+        <header styleName="header">
+          <img src={logo} styleName="logo" alt="logo" />
+          <h1 styleName="title">Welcome to React</h1>
         </header>
-        <p className={styles.intro}>
+        <p styleName="intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
@@ -19,4 +21,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default CssModules(App, styles);
