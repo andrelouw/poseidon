54adc9bf39a08fdaaa44e4bc7dce536bb2a3cd2b
Andre Louw <alouw@retrorabbit.co.za>
2018-03-31 16:02:16 +0200

    Fix cosmos and apply font loaders to prod file.

--- config/cosmos.config.js
+++ config/cosmos.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   rootPath: '../',
   // containerQuerySelector: '#root',
-  globalImports: ['bootstrap/dist/css/bootstrap.min.css', './src/index.scss'],
+  globalImports: ['bootstrap/dist/css/bootstrap.min.css', './src/index.sass'],
   webpackConfigPath: './config/webpack.config.dev',
   publicPath: 'public',
   // Optional: Add this when you start using proxies
