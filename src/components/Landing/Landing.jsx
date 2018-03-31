15a16f28adc145c32b37a79d94f257e422a82b69
Andre Louw <alouw@retrorabbit.co.za>
2018-03-31 21:29:14 +0200

    Add timer.

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,25 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Col, Row } from 'reactstrap';
 import CssModules from 'react-css-modules';
 
 import styles from './Landing.sass';
 
-const landing = () => (
-  <div styleName="header">
-    <Container>
-      <Row>
-        <Col>
-          <div styleName="landing">
-            <div styleName="intro">
-              <h1>Dit is troutyd!</h1>
-              <p styleName="title">Armann & Micheline</p>
-              <p styleName="timer">Timer here</p>
-            </div>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  </div>
-);
+class Landing extends Component {
+  constructor() {
+    super();
+    this.state = {
+      time: {},
+      daysLeft: false,
+    };
+    this.tick = this.tick.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+  }
 
-export default CssModules(landing, styles);
+  componentDidMount() {
+    this.startTimer();
+  }
+
+  countDownDate = new Date('Aug 31, 2018 15:00:00').getTime();
+  countDownTimer = 0;
+
+  tick() {
+    const now = new Date().getTime();
+    const distance = this.countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    this.setState({
+      time: {
+        days,
+        hours,
+        minutes,
+        seconds,
+      },
+    });
+
+    if (distance < 0) {
+      clearInterval(this.countDownTimer);
+      this.setState({ daysLeft: false });
+    }
+  }
+
+  startTimer() {
+    if (this.countDownTimer === 0) {
+      this.countDownTimer = setInterval(this.tick, 1000);
+      this.setState({ daysLeft: true });
+    }
+  }
+
+  render() {
+    let days;
+    if (this.state.daysLeft && this.state.time.days) {
+      days = <p styleName="timer">{this.state.time.days} dae oor!</p>;
+    }
+
+    return (
+      <div styleName="header">
+        <Container>
+          <Row>
+            <Col>
+              <div styleName="landing">
+                <div styleName="intro">
+                  <h1>Dit is troutyd!</h1>
+                  <p styleName="title">Armann & Micheline</p>
+                  {days}
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default CssModules(Landing, styles);
