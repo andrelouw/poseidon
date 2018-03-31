9a934a4a5a7bb9cafe4605f89a292b6a3c1dcc01
Andre Louw <alouw@retrorabbit.co.za>
2018-03-31 12:57:35 +0200

    Setup initial landing pages.
    Still a long way to go.

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Col, Row } from 'reactstrap';
+
+const landing = () => (
+  <Row>
+    <Col>
+      <h1>Dit is troutyd!</h1>
+      <p>Armann & Micheline</p>
+      <p>Timer here</p>
+    </Col>
+  </Row>
+);
+
+export default landing;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,34 +1,17 @@
 import React, { Component } from 'react';
-import CssModules from 'react-css-modules';
-import { Container, Row, Col, Button } from 'reactstrap';
+import { Container } from 'reactstrap';
 
-import logo from '../logo.svg';
-import styles from './App.scss';
+import Landing from '../components/Landing/Landing';
 
 // eslint-disable-next-line
 class App extends Component {
   render() {
     return (
-      <div styleName="App">
-        <header styleName="header">
-          <img src={logo} styleName="logo" alt="logo" />
-          <h1 styleName="title">Welcome to React</h1>
-        </header>
-        <p styleName="intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Container>
-          <Row>
-            <Col md="3"><Button color="blue">Hello Button</Button></Col>
-            <Col md="3">Hello1</Col>
-            <Col md="3">Hello1</Col>
-            <Col md="3">Hello1</Col>
-          </Row>
-        </Container>
-
-      </div>
+      <Container>
+        <Landing />
+      </Container>
     );
   }
 }
 
-export default CssModules(App, styles);
+export default App;
