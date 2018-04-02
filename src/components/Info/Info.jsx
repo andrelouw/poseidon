92433d244e0dab5f311251d3d07142fc3d46daaa
Andre Louw <alouw@retrorabbit.co.za>
2018-04-02 09:51:17 +0200

    Create Info component.
    Bare component.

---
+++ src/components/Info/Info.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line
+class Info extends Component {
+  render() {
+    return (
+      <h1>Info</h1>
+    );
+  }
+}
+
+export default Info;
