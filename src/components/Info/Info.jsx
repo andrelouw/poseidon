ba346a33ddcd4b13cc59ae092671ff40b4615011
Andre Louw <alouw@retrorabbit.co.za>
2018-05-05 10:49:47 +0200

    Add in assets for the info card.
    Extend the card component to take a image, and remove the icon circle.
    These style chagnes were applied in the global style sheet.

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -4,15 +4,33 @@ import { Container, Row } from 'reactstrap';
 
 import styles from './Info.sass';
 import Card from '../shared/Card/Card';
+import calendar from '../../assets/svg/calendar.svg';
+import church from '../../assets/svg/church.svg';
+import rsvp from '../../assets/svg/rsvp.svg';
+
 
 class Info extends Component {
   constructor() {
     super();
     this.state = {
       infoCards: [
-        { id: 1, title: '31 . 08 . 2018', content: '15:00 stap die bruid in… hopelik!' },
-        { id: 2, title: 'Lace on Timber' },
-        { id: 3, title: 'RSVP', content: 'teen 31.07.2018' },
+        {
+          id: 1,
+          title: '31 . 08 . 2018',
+          content: '15:00 stap die bruid in… hopelik!',
+          icon: calendar,
+        },
+        {
+          id: 2,
+          title: 'Lace on Timber',
+          icon: church,
+        },
+        {
+          id: 3,
+          title: 'RSVP',
+          content: 'teen 31.07.2018',
+          icon: rsvp,
+        },
       ],
     };
   }
@@ -23,6 +41,7 @@ class Info extends Component {
         key={card.id}
         title={card.title}
         content={card.content}
+        icon={card.icon}
       />
     ));
 
